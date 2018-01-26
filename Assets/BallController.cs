84e862ad1fcb493d728993c6c2e8626ce3b91da6
Karol Wójcik <blog@karolwojcik.net>
2018-01-26 07:02:03 +0100

    Projekt do filmu www.karolwojcik.net/pierwsza-gra-20-minut

---
+++ Assets/BallController.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallController : MonoBehaviour {
+
+	void OnCollisionEnter2D(Collision2D col){
+		if(col.gameObject.CompareTag("Brick2")){
+			Destroy(col.gameObject);
+		}
+	}
+}
---
+++ Assets/GameController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour {
+
+	public Rigidbody2D ball;
+	public float ballSpeed;
+
+	public Rigidbody2D player;
+	public float playerSpeed;
+
+	private float direction;
+
+	void Start () {
+		ball.AddForce (new Vector2 (1, 1) * ballSpeed);
+	}
+	
+
+	void Update () {
+		direction = Input.GetAxisRaw ("Horizontal");
+		player.velocity = new Vector2 (direction, 0) * playerSpeed;
+	}
+}
